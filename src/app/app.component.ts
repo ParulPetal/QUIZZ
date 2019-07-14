2cf1c5ecf29b1f90c64310338bebffb941d697ec
ParulPetal <parulsingh550@gmail.com>
2019-07-14 22:44:08 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,153 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'quiz';
+
+quiz=function(){
+ let Q=document.getElementById("box");
+ if (Q.style.display== "block")
+{
+Q.style.display="none";
 }
+else{
+
+  Q.style.display="block";
+}
+}
+
+ 
+ans1(){
+alert("Correct ans is 46.");
+};
+
+ans2(){
+  alert("Correct ans is let.");
+  };
+
+  ans3(){
+
+    alert("Correct ans is Um no.");
+};
+
+  ans4(){
+
+    alert(" Correct ans is div.");
+
+  }
+  
+
+clickMessage1 = '';
+//question 1
+
+  Q1A1() {
+    this.clickMessage1 = 'You have selected 46.';
+  }
+ Q1A2() {
+    this.clickMessage1 = 'You have selected 10.';
+  }
+  Q1A3() {
+    this.clickMessage1 = 'You have selected 226.';
+  }
+ Q1A4() {
+    this.clickMessage1 = 'You have selected None.';
+  }
+
+//question 2
+  
+clickMessage2="";
+  Q2A1(){
+    this.clickMessage2 = 'You have selected None.';
+  }
+
+  Q2A2(){
+    this.clickMessage2 = 'You have selected var.';
+  }
+
+  Q2A3(){
+    this.clickMessage2 = 'You have selected All of these.';
+  }
+
+  Q2A4(){
+    this.clickMessage2 = 'You have selected let.';
+  }
+
+
+//question 3
+
+clickMessage3="";
+Q3A1(){
+  this.clickMessage3 = 'You have selected Kinda.';
+}
+Q3A2(){
+  this.clickMessage3 = 'You have selected YES!!!.';
+}
+Q3A3(){
+  this.clickMessage3 = 'You have selected Um no.';
+}
+Q3A4(){
+  this.clickMessage3 = 'You have selected IDK.';
+}
+
+
+
+//question4
+clickMessage4="";
+
+Q4A1(){
+  this.clickMessage4 = 'You have selected span.';
+}
+Q4A2(){
+  this.clickMessage4 = 'You have selected div!!!.';
+}
+Q4A3(){
+  this.clickMessage4 = 'You have selected anchor.';
+}
+Q4A4(){
+  this.clickMessage4 = 'You have selected image.';
+}
+
+   questions = [
+    {
+      question: 'What is 2 + 2 + "6" ?',
+      answers: [
+        { text: '46', correct: true },
+        { text: '10', correct: false },
+        { text: '226', correct: false },
+        { text: 'None', correct: false }
+      ]
+    },
+    {
+      question: 'Which of the following has block scope ?',
+      answers: [
+        { text: 'None', correct: false },
+        { text: 'var ', correct: false },
+        { text: 'All of these', correct: false },
+        { text: 'let', correct: true }
+      ]
+    },
+    {
+      question: 'Is css case sensitive?',
+      answers: [
+        { text: 'Kinda', correct: false },
+        { text: 'YES!!!', correct: false },
+        { text: 'Um no', correct: true },
+        { text: 'IDK', correct: false }
+      ]
+    },
+    {
+      question: 'Which of the following is a block elements?',
+      answers: [
+        { text: 'span', correct: false },
+        { text: 'div', correct: true },
+        { text: 'anchor', correct: false },
+        { text: 'image', correct: false }
+      ]
+    }
+  ]
+  
+  finish(){
+
+alert("Welldone! :)");
+  }
+
+ 
+  }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule} from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ResultComponent } from './result/result.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResultComponent,
+  
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule,FormsModule,
+    AppRoutingModule,RouterModule.forRoot([
+    {
+      path:'',
+      component:AppComponent
+      },
+      {
+      path:'result',
+      component:ResultComponent
+      }
+
+
+
+
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
