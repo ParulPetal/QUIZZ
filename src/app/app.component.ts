0b1c9e526dc9a91db1e2bf385d4a2b36ec303b9f
ParulPetal <parulsingh550@gmail.com>
2019-08-20 20:45:55 +0530

    meassage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {FormControl, FormGroup} from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -7,123 +7,84 @@ import {FormControl, FormGroup} from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+option:any;
+correctData:string;
+counter:number=0;
+constructor(){
+}
 
-quiz=function(){
- let Q=document.getElementById("box");
- if (Q.style.display== "block")
-{
-Q.style.display="none";
+setdata(ans:string){
+this.correctData=ans;
+console.log(this.correctData);
+}
+
+
+
+correctans(x:number){
+  
+if(this.correctData==="46"){
+ console.log(this.correctData);
+//alert("That's correct");
+this.counter=this.counter+1;
+
+}
+else if( this.correctData==="let") {
+  //alert("It's correct");
+  console.log(this.correctData);
+  this.counter=this.counter+1;
+}
+
+else if( this.correctData==='Um no') {
+  //alert("It's correct");
+  console.log(this.correctData);
+  this.counter=this.counter+1;
+}
+
+else if( this.correctData==='div') {
+ // alert("It's correct");
+  this.counter=this.counter+1;
+  
+}
+
+else if( this.correctData==='datalist') {
+  // alert("It's correct");
+   this.counter=this.counter+1;
+   
 }
 else{
 
-  Q.style.display="block";
+ // alert("Incorrect");
 }
+
 }
 
- 
-ans1(){
-alert("Correct ans is 46.");
-};
 
-ans2(){
-  alert("Correct ans is let.");
-  };
 
-  ans3(){
 
-    alert("Correct ans is Um no.");
-};
 
-  ans4(){
 
-    alert(" Correct ans is div.");
 
-  }
+
   
 
-clickMessage1 = '';
-//question 1
 
-  Q1A1() {
-    this.clickMessage1 = 'You have selected 46.';
-  }
- Q1A2() {
-    this.clickMessage1 = 'You have selected 10.';
-  }
-  Q1A3() {
-    this.clickMessage1 = 'You have selected 226.';
-  }
- Q1A4() {
-    this.clickMessage1 = 'You have selected None.';
-  }
-
-//question 2
-  
-clickMessage2="";
-  Q2A1(){
-    this.clickMessage2 = 'You have selected None.';
-  }
-
-  Q2A2(){
-    this.clickMessage2 = 'You have selected var.';
-  }
-
-  Q2A3(){
-    this.clickMessage2 = 'You have selected All of these.';
-  }
-
-  Q2A4(){
-    this.clickMessage2 = 'You have selected let.';
-  }
-
-
-//question 3
-
-clickMessage3="";
-Q3A1(){
-  this.clickMessage3 = 'You have selected Kinda.';
-}
-Q3A2(){
-  this.clickMessage3 = 'You have selected YES!!!.';
-}
-Q3A3(){
-  this.clickMessage3 = 'You have selected Um no.';
-}
-Q3A4(){
-  this.clickMessage3 = 'You have selected IDK.';
-}
-
-
-
-//question4
-clickMessage4="";
-
-Q4A1(){
-  this.clickMessage4 = 'You have selected span.';
-}
-Q4A2(){
-  this.clickMessage4 = 'You have selected div!!!.';
-}
-Q4A3(){
-  this.clickMessage4 = 'You have selected anchor.';
-}
-Q4A4(){
-  this.clickMessage4 = 'You have selected image.';
-}
 
    questions = [
     {
       question: 'What is 2 + 2 + "6" ?',
       answers: [
+        {text: "None selected",correct: false},
         { text: '46', correct: true },
         { text: '10', correct: false },
         { text: '226', correct: false },
-        { text: 'None', correct: false }
+        { text: 'None', correct: false },
+        
       ]
     },
     {
       question: 'Which of the following has block scope ?',
       answers: [
+        {text: "None selected",correct: false},
         { text: 'None', correct: false },
         { text: 'var ', correct: false },
         { text: 'All of these', correct: false },
@@ -133,6 +94,7 @@ Q4A4(){
     {
       question: 'Is css case sensitive?',
       answers: [
+        {text: "None selected",correct: false},
         { text: 'Kinda', correct: false },
         { text: 'YES!!!', correct: false },
         { text: 'Um no', correct: true },
@@ -142,18 +104,38 @@ Q4A4(){
     {
       question: 'Which of the following is a block elements?',
       answers: [
+        {text: "None selected",correct: false},
         { text: 'span', correct: false },
         { text: 'div', correct: true },
         { text: 'anchor', correct: false },
         { text: 'image', correct: false }
       ]
+    },
+    {
+      question: 'Select the new feature of HTML5 ?',
+      answers: [
+        {text: "None selected",correct: false},
+        { text: 'absolute', correct: false },
+        { text: 'datalist', correct: false },
+        { text: 'span', correct: true },
+        { text: 'z-index', correct: false }
+      ]
     }
   ]
   
-  finish(){
 
-alert("Welldone! :)");
+  result(){
+    
+
+    alert("Congratulation on completion. Your Score is "+this.counter+" out of 5.The correct ans are 46,let,Um no,div,datalist");
+
+     }
+    
+
+
+
+
   }
-
+    
  
-  }
+  
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,32 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule} from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ResultComponent } from './result/result.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ResultComponent,
+    
   
   ],
   imports: [
-    BrowserModule,FormsModule,
+    BrowserModule,FormsModule,ReactiveFormsModule,FormsModule,
     AppRoutingModule,RouterModule.forRoot([
     {
       path:'',
       component:AppComponent
       },
-      {
-      path:'result',
-      component:ResultComponent
-      }
-
-
-
-
+      
     ])
   ],
   providers: [],
